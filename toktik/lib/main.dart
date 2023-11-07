a0e49206966e8b70cf30c8c64a29b76c1fb71a38
Jaime benitez <jaime.benitez@alten.es>
2023-11-07 16:18:56 +0100

    Sección 8 - Clean Architecture

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infraestructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infraestructure/reposiories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,12 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDatasource: LocalVideoDatasource()
+      );
+
+
     return MultiProvider(
       providers: [
         //..loadNextPage <- Esto es un operador de cascada para hacer diferentes cosas una tras otra con una misma raiz
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage() 
+          create: (_) => DiscoverProvider(videosRepository: videoPostRepository)..loadNextPage() 
           )
       ],
       child: MaterialApp(
