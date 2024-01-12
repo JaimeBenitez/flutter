9e93fb9c347e8e89d51c44f95f7395742109fddf
Jaime benitez <jaime.benitez@alten.es>
2024-01-12 14:11:08 +0100

    Peliculas individuales y actores - Seccion 14

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListview extends StatefulWidget {
 
@@ -93,10 +94,16 @@ class _Slide extends StatelessWidget {
                 width: 150,
                 loadingBuilder: (context, child, loadingProgress) {
                   if( loadingProgress != null){
-                    return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+                    return const Padding(
+                      padding: EdgeInsets.all(0.8),
+                      child: Center(child: CircularProgressIndicator(strokeWidth: 2)));
                   }
 
-                  return FadeIn(child: child);
+                  return GestureDetector(
+                    onTap: () => context.push('/movie/${movie.id}'),
+                    child: FadeIn(child: child)
+                  );
+                  
                 },
               )
             )
